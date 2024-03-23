afeec4b6a9bf2845596aab9eed71deb49c9a326a
ganeshpc007 <ganeshpc0077@gmail.com>
2024-03-24 00:46:21 +0530

    Routes implimentation done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Chat from "./pages/Chat";
+import Container from "@mui/material/Container";
+import { AuthContext } from "./context/AuthContext";
 
-function App() {
+const App = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/chat");
+    }
+  }, [user, navigate]);
+
   return (
-    <>
-      <SignUp />
-      {/* <SignIn /> */}
-    </>
+    <Container>
+      <Routes>
+        <Route
+          path="/chat"
+          element={user ? <Chat /> : <Navigate to="/signin" />}
+        />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/signin" />} />
+      </Routes>
+    </Container>
   );
-}
+};
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
