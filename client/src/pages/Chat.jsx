ea7f6284b3bd2f71764849c2e2b49b05bad714e6
ganeshpc007 <ganeshpc0077@gmail.com>
2024-03-24 00:47:05 +0530

    Added some components to main chat section

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React from "react";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import { styled } from "@mui/material/styles";
+import profileAvatar from "../assets/avatar.svg";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Paper from "@mui/material/Paper";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 const Chat = () => {
-  return (
-    <div>
-      chat page
-    </div>
-  )
-}
+  const [value, setValue] = React.useState(0);
 
-export default Chat
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Container>
+      <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <Avatar alt="Profile Avatar " src={profileAvatar} />
+        </StyledBadge>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px",
+            display: "flex",
+            alignItems: "center",
+            width: 250,
+            // borderRadius: 10,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search contacts.."
+            // inputProps={{ "aria-label": "search google maps" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <Badge color="secondary" badgeContent={"02"} max={5}>
+          <NotificationsIcon />
+        </Badge>
+      </Stack>
+      <Stack>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="icon label tabs example"
+        >
+          <Tab label="PERSONAL" />
+          <Tab label="ALL" />
+          <Tab label="GROUPS" />
+        </Tabs>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Chat;
