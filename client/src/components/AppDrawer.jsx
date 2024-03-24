ad81cd8cc0d8fde0377d2fddb20c71272075b75e
ganeshpc007 <ganeshpc0077@gmail.com>
2024-03-25 00:01:47 +0530

    App drawer added

---
+++ client/src/components/AppDrawer.jsx
@@ -0,0 +1,129 @@
+import { useContext, useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import { styled, useTheme } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import profileAvatar from "../assets/avatar.svg";
+import { Stack, Typography } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import { AuthContext } from "../context/AuthContext";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+const AppDrawer = ({ open, toggleDrawer }) => {
+  const [logoutModelOpen, setLogoutModelOpen] = useState(false);
+  const { user, siginOut } = useContext(AuthContext);
+
+  const handleClicklogoutModelOpen = () => {
+    setLogoutModelOpen(true);
+  };
+
+  const handleClicklogoutModelClose = () => {
+    setLogoutModelOpen(false);
+  };
+
+  const LogoutConfirm = (
+    <Dialog
+      open={logoutModelOpen}
+      onClose={handleClicklogoutModelOpen}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"Are you sure you want to sign out?"}
+      </DialogTitle>
+
+      <DialogActions>
+        <Button onClick={handleClicklogoutModelClose}>Cancel</Button>
+        <Button onClick={siginOut} autoFocus>
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+
+  const DrawerList = (
+    <Box
+      sx={{
+        width: 250,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+      role="presentation"
+    >
+      <Stack sx={{ width: "100%", alignItems: "center", marginTop: 2 }}>
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <Avatar
+            alt="Profile Avatar"
+            src={profileAvatar}
+            sx={{ cursor: "pointer", width: 80, height: 80, padding: 2 }}
+          />
+        </StyledBadge>
+        <Typography variant="h6" component="div" sx={{ marginTop: 1 }}>
+          {user?.name && user.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user?.email && user.email}
+        </Typography>
+      </Stack>
+      <Stack sx={{ bgcolor: "lightgray", p: 1, borderRadius: 1 }}>
+        <Button
+          startIcon={<LogoutIcon />}
+          // variant="contained"
+          color="error"
+          onClick={handleClicklogoutModelOpen}
+        >
+          Sign out
+        </Button>
+      </Stack>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      {LogoutConfirm}
+    </div>
+  );
+};
+
+export default AppDrawer;
