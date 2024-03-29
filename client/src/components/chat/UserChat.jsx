fbbe28b8770a1d37ba80b0479120cd9fa142e370
ganeshpc007 <ganeshpc0077@gmail.com>
2024-03-30 00:28:34 +0530

    display latest message, time and avart as per name with background color

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,15 +1,124 @@
 import React from "react";
-import { Avatar, Box, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Stack,
+  Typography,
+  AvatarGroup,
+  Badge,
+} from "@mui/material";
 import { useFetchRecipients } from "../../hooks/useFetchRecipients";
+import { useFetchLatestMessage } from "../../hooks/useFetchLatestMessage";
+import moment from "moment";
 
 const UserChat = ({ chat, user }) => {
   const { recipientUsers, loading } = useFetchRecipients(chat, user);
+  const { latestMessage } = useFetchLatestMessage(chat);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  console.log("recipientUsers", recipientUsers);
+  const stringToColor = (string) => {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  };
+
+  const stringAvatar = (name) => {
+    const words = name.split(/[ -]/);
+
+    let letters = "";
+    if (words.length === 1) {
+      letters = words[0].charAt(0).toUpperCase();
+    } else {
+      letters =
+        words[0].charAt(0).toUpperCase() + words[1].charAt(0).toUpperCase();
+    }
+
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: letters,
+    };
+  };
+
+  // console.log("recipientUsers", recipientUsers);
+
+  const getAvatar = () => {
+    if (chat?.members?.length > 2) {
+      return (
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          badgeContent={
+            <span
+              style={{
+                argin: "1px",
+                borderRadius: "50%",
+                backgroundColor: "black",
+                color: "white",
+                padding: "1px",
+                height: "13px",
+                width: "13px",
+                fontSize: "7px",
+                lineHeight: "13px",
+                textAlign: "center",
+              }}
+            >
+              TM
+            </span>
+          }
+        >
+          {chat?.teamName && (
+            <Avatar
+              sx={{ width: 33, height: 33 }}
+              {...stringAvatar(chat?.teamName)}
+            />
+          )}
+        </Badge>
+      );
+    }
+    return (
+      recipientUsers[0]?.name && (
+        <Avatar
+          sx={{ width: 33, height: 33 }}
+          {...stringAvatar(recipientUsers[0]?.name)}
+        />
+      )
+    );
+    // <Avatar sx={{ width: 33, height: 33 }}>
+    //   {recipientUsers[0]?.name &&
+    //     getProfilePicLetter(recipientUsers[0]?.name)}
+    // </Avatar>
+  };
+
+  const getChatNeme = () => {
+    if (chat?.members?.length > 2) {
+      return chat.teamName;
+    }
+    return recipientUsers[0]?.name;
+  };
+
+  const truncateText = (text) => {
+    return text?.length > 20 ? text.substr(0, 20) + "..." : text;
+  };
+
   return (
     <Box
       sx={{
@@ -34,20 +143,27 @@ const UserChat = ({ chat, user }) => {
         direction="row"
         spacing={2}
         sx={{
-          width: "90%",
+          width: "65%",
           alignItems: "center",
         }}
       >
-        <Avatar sx={{ width: 33, height: 33 }}>H</Avatar>
+        {getAvatar()}
         <Box>
-          <Typography variant="subtitle2">Ashii</Typography>
+          <Typography variant="subtitle2">{getChatNeme()}</Typography>
           <Typography variant="body2" sx={{ color: "rgb(96, 92, 92)" }}>
-            Hi appi chekc out this code
+            {latestMessage?.text
+              ? truncateText(latestMessage?.text)
+              : "Start conversation.."}
           </Typography>
         </Box>
       </Stack>
-      <Typography variant="body2" color="text.secondary">
-        12/03/2024
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        sx={{ fontSize: "12px" }}
+      >
+        {latestMessage?.createdAt &&
+          moment(latestMessage?.createdAt).calendar()}
       </Typography>
     </Box>
   );
