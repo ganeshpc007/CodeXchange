fb2cd5116264e3359c26a34ecfbc0b1bd432db1f
ganeshpc007 <ganeshpc0077@gmail.com>
2024-04-01 00:22:13 +0530

    monaco-editor used to display code

--- client/src/components/CodeDisplay.jsx
+++ client/src/components/CodeDisplay.jsx
@@ -1,28 +1,91 @@
-import React, { useEffect } from "react";
-import hljs from "highlight.js";
-import "highlight.js/styles/github.css"; // Import a code highlighting theme
+// import React, { useEffect } from "react";
+// import hljs from "highlight.js";
+// import "highlight.js/styles/github.css"; // Import a code highlighting theme
+
+// const CodeDisplay = ({ code, language }) => {
+//   useEffect(() => {
+//     hljs.highlightAll(); // Apply syntax highlighting on component mount
+//   }, []);
+
+//   return (
+//     <pre style={{ margin: 0 }}>
+//       <code
+//         style={{
+//           background: "black",
+//           color: "white",
+//           width: "625px",
+//           fontSize: "14px",
+//           overflowX: "scroll",
+//         }}
+//         className={`language-${language}`}
+//         dangerouslySetInnerHTML={{
+//           __html: hljs.highlight(code, { language }).value,
+//         }}
+//       />
+//     </pre>
+//   );
+// };
+
+// export default CodeDisplay;
+
+// import Editor from "@monaco-editor/react";
+// const CodeDisplay = ({ code, language }) => {
+//   return (
+//     <Editor
+//       height={"250px"}
+//       language={language}
+//       theme="vs-dark"
+//       value={code}
+//       options={{
+//         inlineSuggest: true,
+//         fontSize: "16px",
+//         formatOnType: true,
+//         autoClosingBrackets: true,
+//         minimap: { scale: 1 },
+//         automaticLayout: true,
+//         matchBrackets: "always",
+//       }}
+//     />
+//   );
+// };
+
+// export default CodeDisplay;
+
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
 
 const CodeDisplay = ({ code, language }) => {
-  useEffect(() => {
-    hljs.highlightAll(); // Apply syntax highlighting on component mount
-  }, []);
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  const toggleScroll = () => {
+    setScrollEnabled((prevScrollEnabled) => !prevScrollEnabled);
+  };
 
   return (
-    <pre style={{ margin: 0 }}>
-      <code
-        style={{
-          background: "black",
-          color: "white",
-          width: "625px",
-          fontSize: "14px",
-          overflowX: "scroll",
-        }}
-        className={`language-${language}`}
-        dangerouslySetInnerHTML={{
-          __html: hljs.highlight(code, { language }).value,
+    <div
+      style={{
+        overflowY: scrollEnabled ? "auto" : "hidden",
+        height: "250px",
+        pointerEvents: scrollEnabled ? "auto" : "none",
+      }}
+      onDoubleClick={toggleScroll}
+    >
+      <Editor
+        height={"100%"}
+        language={language}
+        theme="vs-dark"
+        value={code}
+        options={{
+          inlineSuggest: true,
+          fontSize: "16px",
+          formatOnType: true,
+          autoClosingBrackets: true,
+          minimap: { scale: 1 },
+          automaticLayout: true,
+          matchBrackets: "always",
         }}
       />
-    </pre>
+    </div>
   );
 };
 
