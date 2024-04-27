cc40e49a13abac93012f9767122763e5a32ba92b
ganeshpc007 <ganeshpc0077@gmail.com>
2024-04-28 00:18:22 +0530

    vercel analytics components utlized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -10,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <AuthContextProvider>
         <App />
+        <Analytics />
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
