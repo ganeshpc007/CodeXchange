9dc9fa8144e9a55d290b2c09ef57ffd895a6dc63
ganeshpc007 <ganeshpc0077@gmail.com>
2024-05-16 23:54:34 +0530

    updated with new server URL

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,5 +1,6 @@
 // export const baseUrl = "http://localhost:5000/api";
-export const baseUrl = "https://server-codexchange.onrender.com/api";
+// export const baseUrl = "https://server-codexchange.onrender.com/api";
+export const baseUrl = "https://xcode-server.vercel.app/api";
 
 export const postRequest = async (url, body) => {
   const response = await fetch(url, {
